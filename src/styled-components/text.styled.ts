e99805eddc3a88946398e5505fc721ff75fd494f
Camilo Vargas <camilovargasingm@gmail.com>
2023-03-09 23:19:32 -0500

    add default text

---
+++ src/styled-components/text.styled.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  color: black;
+  margin: 0;
+  font-size: 1.5rem;
+`;
+export const H2 = styled.h2`
+  color: black;
+  margin: 0;
+  font-size: 1.25rem;
+`;
+export const H3 = styled.h3`
+  color: black;
+  margin: 0;
+  font-size: 1.15rem;
+`;
+export const H4 = styled.h4`
+  color: black;
+  margin: 0;
+  font-size: 1rem;
+`;
+
+export const P = styled.p`
+  color: black;
+  margin: 0;
+  font-size: 1rem;
+`;
