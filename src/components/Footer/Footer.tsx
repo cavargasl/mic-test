223b4b961579408da9ec7e29d0539b41b621332b
Camilo Vargas <camilovargasingm@gmail.com>
2023-03-10 21:23:03 -0500

    create banner footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { InfoClient } from './InfoClient'
 import { Newsletter } from './Newsletter'
-import { LayoutFooter } from './styled-components'
+import { LayoutFooter, PaymentMethods } from './styled-components'
 
 export default function Footer() {
   return (
     <LayoutFooter>
       <InfoClient />
       <Newsletter />
+      <PaymentMethods src='https://moviesshopco.vteximg.com.br/arquivos/medios-de-pago-mic-footer.png' alt='payment methods' />
     </LayoutFooter>
   )
 }
--- src/components/Footer/styled-components/footer.styled.ts
+++ src/components/Footer/styled-components/footer.styled.ts
@@ -1,3 +1,4 @@
+import { Image } from 'src/styled-components';
 import styled from 'styled-components';
 
 export const LayoutFooter = styled.footer`
@@ -12,5 +13,14 @@ export const LayoutFooter = styled.footer`
   }
   @media only screen and (max-width: 1235px) {
     width: 80%;
+  }
+`;
+
+export const PaymentMethods = styled(Image)`
+  max-width: 70%;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 70rem) {
+    max-width: 90%;
   }
 `;
