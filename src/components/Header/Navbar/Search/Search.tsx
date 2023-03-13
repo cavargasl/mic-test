5f7954efc7339b7b4b348576f4d2ac1cf29e84f0
Camilo Vargas <camilovargasingm@gmail.com>
2023-03-13 12:37:51 -0500

    correction search component

--- src/components/Header/Navbar/Search/Search.tsx
+++ src/components/Header/Navbar/Search/Search.tsx
@@ -1,12 +1,13 @@
 import { SearchInput } from './styled-components'
 import Icon from '@mdi/react';
 import { mdiMagnify } from '@mdi/js';
+import { Text } from '@chakra-ui/react';
 
 export default function Search() {
   return (
-    <SearchInput>
-      <span style={{ whiteSpace: "nowrap" }}>Encuentra tu preferido</span>
-      <Icon path={mdiMagnify} size={"15px"} />
+    <SearchInput >
+      <Text as="span" whiteSpace={"nowrap"} color="white">Encuentra tu preferido</Text>
+      <Icon path={mdiMagnify} size={"15px"} color='white' />
     </SearchInput>
   )
 }
