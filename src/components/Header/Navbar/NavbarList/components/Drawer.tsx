a3ed47d719caa7ec2a8da8d313ec610d83584352
Camilo Vargas <camilovargasingm@gmail.com>
2023-03-13 20:49:17 -0500

    create submenus mobile

--- src/components/Header/Navbar/NavbarList/components/Drawer.tsx
+++ src/components/Header/Navbar/NavbarList/components/Drawer.tsx
@@ -1,4 +1,7 @@
-import { Drawer as DrawerChakra, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Text, VStack } from '@chakra-ui/react'
+import { Button, Drawer as DrawerChakra, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Text, VStack } from '@chakra-ui/react'
+import { mdiBackburger } from '@mdi/js'
+import Icon from '@mdi/react'
+import { useState } from 'react'
 import { ListIcons } from '../../ListIcons'
 import { MenuList } from '../models'
 
@@ -8,6 +11,13 @@ interface Props {
 }
 
 export default function Drawer({ isOpen, onClose }: Props) {
+  const [subMenu, setSubMenu] = useState<MenuList["subMenu"]>()
+
+  function handleClick(data: MenuList["subMenu"]) {
+    if (data) return setSubMenu(data)
+    return setSubMenu(undefined)
+  }
+
   return (
     <DrawerChakra
       isOpen={isOpen}
@@ -21,11 +31,23 @@ export default function Drawer({ isOpen, onClose }: Props) {
         <DrawerHeader textTransform={"uppercase"} fontSize="2rem" fontWeight={600} borderBottom="1px solid">¡Hola!</DrawerHeader>
 
         <DrawerBody paddingBlock={8} display="flex" flexDir={"column"} gap={8}>
-          <VStack gap={6} alignItems="start" borderBottom={"1px solid"} borderColor="blackAlpha.400" paddingBottom={6}>
-            {
-              MenuList.map(item => <Text key={item.title} fontWeight={800} fontSize="1.2rem" cursor={"pointer"}>{item.title}</Text>)
-            }
-          </VStack>
+          {
+            subMenu ?
+              <VStack gap={6} alignItems="start" borderBottom={"1px solid"} borderColor="blackAlpha.400" paddingBottom={6}>
+                <Button variant={"unstyled"} color="red" onClick={() => handleClick(undefined)}>
+                  <Icon path={mdiBackburger} size={2} />
+                </Button>
+                {
+                  subMenu.map(item => <Text key={item} fontWeight={600} cursor={"pointer"}>{item}</Text>)
+                }
+              </VStack>
+              :
+              <VStack gap={6} alignItems="start" borderBottom={"1px solid"} borderColor="blackAlpha.400" paddingBottom={6}>
+                {
+                  MenuList.map(item => <Text key={item.title} fontWeight={800} fontSize="1.2rem" cursor={"pointer"} onClick={() => handleClick(item.subMenu)}>{item.title}</Text>)
+                }
+              </VStack>
+          }
           <ListIcons isMobile />
         </DrawerBody>
 
